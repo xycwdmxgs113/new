b9da7c9e005b9a21b089964906ffde794fd55c93
xycwdmxgs113 <204438535@qq.com>
2024-08-27 15:36:02 +0800

    2024/8/27

---
+++ C++初阶篇/2024_8_26(list)/2024_8_26/2024_8_26/2024_8_26.cpp
@@ -0,0 +1,203 @@
+#include"2024_8_26.h"
+
+
+
+//void test01()
+//{
+//	list<int> v;
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(3);
+//	v.push_back(4);
+//
+//	for (auto e:v)
+//	{
+//		cout << e<<" ";
+//	}
+//	cout << endl;
+//
+//	auto e = v.begin();
+//	for (size_t i = 0; i < 2; i++)
+//	{
+//		++e;
+//	}
+//
+//	v.insert(e, 100);
+//	for (auto e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	e = find(v.begin(), v.end(), 100);
+//	v.insert(e, 200);
+//	for (auto e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//
+//}
+//
+//void test02()
+//{
+//	list<int> v;
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(3);
+//	v.push_back(4);
+//
+//	for (auto e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	
+//	sort(v.begin(), v.end());
+//
+//	for (auto e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	v.sort();
+//	for (auto e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//}
+
+using namespace std;
+
+void test01()
+{
+	mabo::list<int> li;
+	li.push_back(1);
+	li.push_back(2);
+	li.push_back(3);
+	li.push_back(4);
+
+	mabo::list<int>::iterator it = li.begin();
+	while (it!=li.end())
+	{
+		cout << (*it) << " ";
+		++it;
+	}
+	cout << endl;
+
+
+	for (auto e:li)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+}
+using namespace mabo;
+void test02()
+{
+	
+	list<A> li;
+	li.push_back(A(1, 2));
+	li.push_back(A(3, 4));
+	mabo::list<A>::iterator it = li.begin();
+	while (it != li.end())
+	{
+		cout <<it->_a1 << " "<< it->_a2<<endl;
+		++it;
+	}
+	cout << endl;
+
+}
+
+void test03()
+{
+	list<int> lt;
+	lt.push_back(1);
+	lt.push_back(2);
+	lt.push_back(3);
+	lt.push_back(4);
+	lt.push_front(5);
+	lt.push_front(6);
+	lt.push_front(7);
+	lt.push_front(8);
+
+	for (auto e : lt)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	lt.clear();
+	list<int> lt1;
+	lt1.push_back(10);
+	lt1.push_back(20);
+	lt1.push_back(30);
+	lt1.push_back(40);
+	lt = lt1;
+	for (auto e : lt)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+}
+
+
+
+void test04()
+{
+	list<int> lt;
+	lt.push_back(1);
+	lt.push_back(2);
+	lt.push_back(3);
+	lt.push_back(4);
+	for (auto e : lt)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	list<int> lt1 = lt;
+	for (auto e : lt1)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+}
+
+void test05()
+{
+	list<int> lt;
+	lt.push_back(1);
+	lt.push_back(2);
+	lt.push_back(3);
+	lt.push_back(4);
+
+	list<int> lt1;
+	lt1.push_back(5);
+	lt1.push_back(6);
+	lt1.push_back(7);
+	lt1.push_back(8);
+
+	lt = lt1;
+	for (auto e : lt)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+}
+
+int main()
+{
+	test03();
+
+
+
+	return 0;
+}
---
+++ C++初阶篇/2024_8_26(list)/2024_8_26/2024_8_26/2024_8_26.h
@@ -0,0 +1,221 @@
+#pragma once
+#include<iostream>
+#include<stdbool.h>
+#include<assert.h>
+
+
+namespace mabo
+{
+	template<class T>
+	struct list_node
+	{
+		list_node(T val=T())
+			:_next(nullptr)
+			,_prev(nullptr)
+			,_val(val)
+		{}
+
+		T _val;
+		list_node* _next;
+		list_node* _prev;
+	};
+
+	template<class T, class Ref,class Por>
+	struct _list_iterator
+	{
+		typedef list_node<T> Node;
+		typedef _list_iterator<T,Ref,Por> self;
+		_list_iterator(Node*node)
+			:_node(node)
+		{}
+
+		Ref operator*()
+		{
+			return _node->_val;
+		}
+
+		self&operator++()
+		{
+			_node = _node->_next;
+			return *this;
+		}
+
+		self operator++(int)
+		{
+			self temp(*this);
+			_node = _node->_next;
+			return temp;
+		}
+
+		self& operator--()
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+
+		self& operator--(int)
+		{
+			self temp(*this);
+			_node = _node->_prev;
+			return *this;
+		}
+
+		Por operator->()
+		{
+			return  &_node->_val;
+		}
+
+		bool operator!=(const  self& it)const
+		{
+			return _node != it._node;
+		}
+
+		bool operator ==(const  self& it)const
+		{
+			return _node == it._node;
+		}
+
+		Node* _node;
+	};
+
+
+	template<class T>
+	class list
+	{
+		typedef list_node<T> Node;
+	public:
+		typedef _list_iterator<T,T&,T*> iterator;
+		typedef _list_iterator<T,const T&,const T*> const_iterator;
+
+		void emptyInit()
+		{
+			_head = new Node(T());
+			_head->_next = _head->_prev = _head;
+			_size = 0;
+		}
+
+		list()
+		{
+			emptyInit();
+		}
+
+		list(const list<T>&lt)
+		{
+			emptyInit();
+			for (auto& e:lt)
+			{
+				push_back(e);
+			}
+		}
+
+		~list()
+		{
+			clear();
+			delete _head;
+			_head = nullptr;
+			_size = 0;
+		}
+
+		void swap(list<T>& lt)
+		{
+			std::swap(_head, lt._head);
+			std::swap(_size, lt._size);
+		}
+
+		list<T>&operator=(list<T> lt)
+		{
+			swap(lt);
+			return *this;
+		}
+
+		void push_back(const T&val)
+		{
+			insert(end(), val);
+		}
+
+		void push_front(const T& val)
+		{
+			insert(begin(), val);
+		}
+
+
+		iterator begin()
+		{
+			return _head->_next;
+		}
+
+		iterator end()
+		{
+			return _head;
+		}
+
+		const iterator begin() const
+		{
+			return _head->_next;
+		}
+
+		const iterator end() const
+		{
+			return _head;
+		}
+
+		iterator insert(iterator pos, const T& val)
+		{
+			Node* newnode = new Node(val);
+			newnode->_next = pos._node;
+			newnode->_prev = pos._node->_prev;
+			pos._node->_prev->_next = newnode;
+			pos._node->_prev = newnode;
+			_size++;
+			return newnode; 
+		}
+
+		iterator erase(iterator pos)
+		{
+			assert(pos != end());
+			Node* cur = pos._node;
+			Node* prev = cur->_prev;
+			Node* next = cur->_next;
+			
+			prev->_next = next;
+			next->_prev = prev;
+			delete cur;
+			_size--;
+			return next;
+		}
+
+		size_t size()
+		{
+			return _size;
+		}
+
+		void clear()
+		{
+			iterator it = begin();
+			while (it!=end())
+			{
+				it=erase(it);
+			}
+			_size = 0;
+		}
+
+
+	private:
+		Node* _head;
+		size_t _size;
+	};
+
+
+	struct A
+	{
+		A(int a1 = 1, int a2 = 2)
+			:_a1(a1)
+			, _a2(a2)
+		{}
+
+		int _a1;
+		int _a2;
+	};
+
+
+}
